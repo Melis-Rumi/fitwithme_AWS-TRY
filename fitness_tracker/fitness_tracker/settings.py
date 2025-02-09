bc3ff91ff73288f073dee2978b1dd9843e456b44
Melis-Rumi <melis.zhalalov@gmail.com>
2025-02-09 05:42:40 +0000

    superuser3

--- fitness_tracker/fitness_tracker/settings.py
+++ fitness_tracker/fitness_tracker/settings.py
@@ -161,13 +161,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-#STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = '/static/'  # URL to access static files
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Directory where static files are collected
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # Additional directories for static files
-]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
